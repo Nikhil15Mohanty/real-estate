255a720091ad7bf6e554921385b61afab466e194
Nikhil Mohanty <nikhilviki.15@gmail.com>
2024-02-19 23:22:13 +0530

    small progress of header

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Mohanty</span>
+            <span className='text-slate-700'>EState</span>
+        </h1>
+    </header>
+  )
+}
