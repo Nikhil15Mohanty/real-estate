603b9cca399bbd3253d80a8fd592720c7363ef21
Nikhil Mohanty <nikhilviki.15@gmail.com>
2024-02-29 23:26:05 +0530

    create a form handle change command so that we can track the value

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 export const SignUp = () => {
+
+  const [formData,setFormData] = useState({})
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id] :e.target.value
+    })
+  }
+  console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-center text-3xl font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
         <input type="text" placeholder='username'
-          id='username' className='border p-3 rounded-lg outline-none' />
+          id='username' className='border p-3 rounded-lg outline-none'   onChange={handleChange} />
           <input type="email" placeholder='email'
-          id='email' className='border p-3 rounded-lg outline-none' />
+          id='email' className='border p-3 rounded-lg outline-none' onChange={handleChange} />
           <input type="password" placeholder='password'
-          id='password' className='border p-3 rounded-lg outline-none' />
+          id='password' className='border p-3 rounded-lg outline-none' onChange={handleChange} />
           <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 uppercase disabled:opacity-80'>Sign up</button>
       </form>
       <div className='flex gap-2 mt-3'>
